67683ab389bb5db52c1fd458d49c61f6eaed9f9e
YujinJI <yujin1679@naver.com>
2020-02-06 18:59:16 +0900

    add

---
+++ 2-1/2-1/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  2-1
+//
+//  Created by mac on 2019. 9. 9..
+//  Copyright © 2019년 size. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("이름 : 지유진\n");
+    printf("나이 : 20살\n");
+    printf("주소 : 김해시 능동로 118\n");
+    return 0;
+}
---
+++ 2-2/2-2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  2-2
+//
+//  Created by mac on 2019. 9. 9..
+//  Copyright © 2019년 size. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello\nC\nProgrammers!\n");
+    return 0;
+}
